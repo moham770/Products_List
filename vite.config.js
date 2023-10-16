e48774d464a54e119aad307555089879fd64dd4d
mohamed <mohamayman770@gmail,com>
2023-10-17 02:32:58 +0300

    fix: some bugs

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/[Products_List]/"
+  base: "/Products_List/"
 
 })
